b4748a57d594b9d130f24b165667a0fcaf0e9347
evertonmessias <everton.messias@gmail.com>
2020-12-29 13:17:32 -0300

    29-12-2020_13:17:32

--- wp-content/themes/nutricao-renal/home.php
+++ wp-content/themes/nutricao-renal/home.php
@@ -37,13 +37,7 @@
 			</a>
 
 
-		<div class="row">
-
-		<?php the_posts_pagination( array(
-			'mid_size'  => 2,
-			'prev_text' => __( '<', 'textdomain' ),
-			'next_text' => __( '>', 'textdomain' ),
-		) ); ?>
+		<div class="row">	
 
 		</div>
 
