8267696d6a9c6b10ba1f9a09d1cda733d1357f73
evertonmessias <everton.messias@gmail.com>
2021-01-13 19:28:26 -0300

    13-01-2021_19:28:26

--- wp-content/themes/nutricao-renal/page-programa-de-apoio-ao-paciente-gerar-cupom.php
+++ wp-content/themes/nutricao-renal/page-programa-de-apoio-ao-paciente-gerar-cupom.php
@@ -6,13 +6,13 @@
 				<div class="col-lg-12 dados-form">
 					<h1>Gerar Cupom</h1>
 					<?php
-					if(is_user_logged_in()){								
-					echo do_shortcode("[wpforms_entries_table id='1388' fields='1,3,10,31,37']");
-					echo "<br><button class='btngeracod' onclick='geracod()'>Gerar Novo Cupom</button>";
-					}else{
+					if (is_user_logged_in()) {
+						echo do_shortcode("[wpforms_entries_table id='1388' fields='1,3,10,31,37']");
+						echo "<br><button class='btngeracod' onclick='geracod()'>Gerar Novo Cupom</button>";
+					} else {
 						echo "<h3>Precisa estar logado no sistema !</h3>";
 					}
-					?>				
+					?>
 				</div>
 			</div>
 		</div>
