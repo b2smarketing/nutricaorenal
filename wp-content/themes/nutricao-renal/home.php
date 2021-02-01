26849ec5c8cd5b13f97d6a69e8842cf8e0aab852
evertonmessias <everton.messias@gmail.com>
2021-02-01 16:31:56 -0300

    01-02-2021_16:31:56

--- wp-content/themes/nutricao-renal/home.php
+++ wp-content/themes/nutricao-renal/home.php
@@ -6,49 +6,29 @@
 
 	<div class="container-fluid">
 
-			<h1>Keto Connect</h1>	
-
-
-			<?php /* if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-			<a href="<?php the_permalink(); ?>">
-				<div class="row">
-					<div class="box20 wow fadeInLeft" style="background-image: url('<?php the_post_thumbnail_url();?>');">
-					</div>
-					<div class="box80 wow fadeInRight linksNutricaoManejo">
-						<h2><?php the_title();?></h2>
-						<p><?php the_excerpt();?></p>
-					</div>
-				</div>
-			</a>
-
-
-		<?php endwhile; endif;*/ ?>
-
+		<h1>Keto Connect</h1>
+		<div class="row quadro">
 			<a href="/update-kdoqi-2020/">
-				<div class="row">
-					<div class="box20 wow fadeInLeft" style="background-image: url('<?php the_post_thumbnail_url();?>');">
-					</div>
+				<!--<div class="row">
+					<div class="box20 wow fadeInLeft" style="background-image: url('<?php //the_post_thumbnail_url(); ?>');"></div>-->
 					<div class="box80 wow fadeInRight linksNutricaoManejo">
 						<h2>UPDATE KDOQI 2020</h2>
 						<p>Confira os Highlights do KDOQI 2020 – O que há de novo sobre as proteínas no tratamento conservador da DRC, por Lilian Cuppari.</p>
 					</div>
-				</div>
+				<!--</div>-->
 			</a>
-
-
-		<div class="row">	
-
 		</div>
-
+		
+	<!-- DESCOMENTAR AQUI >>>>>>>
+		<div class="row video">
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+	-->
 
 	</div>
-    
-    
-	<?php include ("includes/rins-tabs.php"); ?>
+
+
+	<?php include("includes/rins-tabs.php"); ?>
 </section>
 
-
-
-
-<?php get_footer();?>
+<?php get_footer(); ?>
