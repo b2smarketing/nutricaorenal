06b88ebb5351951bc7a870a0d22a506d6e0de044
evertonmessias <everton.messias@gmail.com>
2021-07-15 16:09:26 -0300

    15-07-2021_16:09:25

--- wp-content/themes/nutricao-renal/includes/rins-tabs.php
+++ wp-content/themes/nutricao-renal/includes/rins-tabs.php
@@ -1,15 +1,15 @@
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-lg-3"><a href="os-rins-funcoes.php">
+		<div class="col-lg-3"><a href="/os-rins-funcoes">
 			<div class="box">Funções<i class="fa fa-angle-right"></i></div></a>
 		</div>	
-		<div class="col-lg-3"><a href="os-rins-anatomia.php">
+		<div class="col-lg-3"><a href="/os-rins-anatomia">
 			<div class="box">Anatomia<i class="fa fa-angle-right"></i></div></a>
 		</div>	
-		<div class="col-lg-3"><a href="os-rins-como-funcionam.php">
+		<div class="col-lg-3"><a href="/os-rins-como-funcionam">
 			<div class="box">Como funcionam?<i class="fa fa-angle-right"></i></div></a>
 		</div>	
-		<div class="col-lg-3"><a href="os-rins-o-que-e-funcao-renal.php">
+		<div class="col-lg-3"><a href="/os-rins-o-que-e-funcao-renal">
 			<div class="box">Função Renal<i class="fa fa-angle-right"></i></div></a>
 		</div>	
 	</div>
